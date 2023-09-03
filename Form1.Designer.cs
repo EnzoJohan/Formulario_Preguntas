04fc06d204d673a0f026eb7c7ede7dc89ec3b657
EnzoJohan <143907383+EnzoJohan@users.noreply.github.com>
2023-09-02 19:38:26 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Formulario_Preguntas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblEquipo = new System.Windows.Forms.Label();
+            this.cmbGenero = new System.Windows.Forms.ComboBox();
+            this.cmbEdad = new System.Windows.Forms.ComboBox();
+            this.cmbEquipo = new System.Windows.Forms.ComboBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Location = new System.Drawing.Point(85, 29);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(96, 13);
+            this.lblGenero.TabIndex = 0;
+            this.lblGenero.Text = "Cual es tu genero?";
+            this.lblGenero.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(313, 29);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(97, 13);
+            this.lblEdad.TabIndex = 1;
+            this.lblEdad.Text = "Tienes mas de 18?";
+            // 
+            // lblEquipo
+            // 
+            this.lblEquipo.AutoSize = true;
+            this.lblEquipo.Location = new System.Drawing.Point(553, 29);
+            this.lblEquipo.Name = "lblEquipo";
+            this.lblEquipo.Size = new System.Drawing.Size(132, 13);
+            this.lblEquipo.TabIndex = 2;
+            this.lblEquipo.Text = "A que equipo perteneces?";
+            // 
+            // cmbGenero
+            // 
+            this.cmbGenero.FormattingEnabled = true;
+            this.cmbGenero.Items.AddRange(new object[] {
+            "Mujer",
+            "Hombre"});
+            this.cmbGenero.Location = new System.Drawing.Point(71, 45);
+            this.cmbGenero.Name = "cmbGenero";
+            this.cmbGenero.Size = new System.Drawing.Size(121, 21);
+            this.cmbGenero.TabIndex = 3;
+            // 
+            // cmbEdad
+            // 
+            this.cmbEdad.FormattingEnabled = true;
+            this.cmbEdad.Items.AddRange(new object[] {
+            "si",
+            "no"});
+            this.cmbEdad.Location = new System.Drawing.Point(297, 45);
+            this.cmbEdad.Name = "cmbEdad";
+            this.cmbEdad.Size = new System.Drawing.Size(121, 21);
+            this.cmbEdad.TabIndex = 4;
+            // 
+            // cmbEquipo
+            // 
+            this.cmbEquipo.FormattingEnabled = true;
+            this.cmbEquipo.Items.AddRange(new object[] {
+            "Frio",
+            "Calor"});
+            this.cmbEquipo.Location = new System.Drawing.Point(556, 45);
+            this.cmbEquipo.Name = "cmbEquipo";
+            this.cmbEquipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbEquipo.TabIndex = 5;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(129, 200);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(242, 105);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(655, 381);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(105, 57);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.cmbEquipo);
+            this.Controls.Add(this.cmbEdad);
+            this.Controls.Add(this.cmbGenero);
+            this.Controls.Add(this.lblEquipo);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblGenero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblEquipo;
+        private System.Windows.Forms.ComboBox cmbGenero;
+        private System.Windows.Forms.ComboBox cmbEdad;
+        private System.Windows.Forms.ComboBox cmbEquipo;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_Preguntas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click_Click(object sender, EventArgs e)
+        {
+            string genero = cmbGenero.SelectedItem.ToString();
+            string edad = cmbEdad.SelectedItem.ToString();
+            string equipo = cmbEquipo.SelectedItem.ToString();
+            string mensaje = $"Género: {genero}\nEdad: {edad}\nEquipo: {equipo}";
+            MessageBox.Show(mensaje, "Respuestas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
